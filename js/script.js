4c1c15f44dff3af0bea0a428211f1a112e03d9ef
nabiedma <nabiedma@gmail.com>
2020-11-01 02:08:03 -0300

    added smooth scroll for anchor

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ function openNav() {
 function closeNav() {
     document.getElementById("sidebar").style.left = "-230px";
 }
+
+// Smooth Scroll
+const links = document.querySelectorAll(".nav-link");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  document.getElementById("sidebar").style.left = "-230px";
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
